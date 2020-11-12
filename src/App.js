17ead8728f5afade87f288d8411eaf74deb0e0f7
Shubhamsg7 <shubhamkumar8028@gmail.com>
2020-11-13 01:05:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min";
+
 
 function App() {
+
+  const [Radius,setRadius]=useState(0);
+  const [Hor,setHor]=useState(10);
+  const [Vert,setVert]=useState(10);
+  const [Blur,setBlur]=useState(10);
+  const [Color,setColor]=useState("grey");
+  const [Check,setCheck]=useState(false);
+
+
   return (
+    <>
+    <h3 className="heading">Box Shadow Generator</h3>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="output">
+        <div className="box" style={{boxShadow:`${Check ? "inset":""} ${Hor}px ${Vert}px ${Blur}px ${Color}`, borderRadius:`${Radius}%`}}>
+        <code className="code">
+          border-radius: {Radius}%;<br/>
+          box-shadow: {Hor}px {Vert}px {Blur}px {Color};
+        </code>
+        </div>
+      </div>
+      <div className="controls" style={{boxShadow:`${Check ? "inset":""} ${Hor}px ${Vert}px ${Blur}px ${Color}`}}>
+      <label>Border Radius</label>
+          <input type="range" value={Radius} onChange={(e)=>setRadius(e.target.value)} min="0" max="100" />
+        <label>Horizontal Length</label>
+          <input type="range" value={Hor} onChange={(e)=>setHor(e.target.value)} min="-100" max="100" />
+        <label>Vertical Length</label>
+          <input type="range" value={Vert} onChange={(e)=>setVert(e.target.value)} min="-100" max="100" />
+        <label>Blur</label>
+          <input type="range" value={Blur} onChange={(e)=>setBlur(e.target.value)} min="0" max="100" />
+          <br /><br />
+        <label>Color : </label>
+          <input type="color" value={Color} onChange={(e)=>setColor(e.target.value)} />
+          <br /><br />
+          <div className="switch">
+            <label>
+              Outline
+              <input type="checkbox" checked={Check} onChange={()=>setCheck(!Check)} />
+              <span className="lever"></span>
+              Inset
+            </label>
+          </div>
+      </div>
     </div>
+    </>
   );
 }
 
